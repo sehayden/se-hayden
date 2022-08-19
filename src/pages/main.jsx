029862236ace4a092b1e0296a580560216238fc2
dnhaa <elnganha@gmail.com>
2022-08-19 23:47:19 +0700

    yet to solve mouse move parallax

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
-import { ReactComponent as H } from './h.svg';
-import { ReactComponent as A } from './a.svg';
+import { MouseParallaxContainer, MouseParallaxChild } from "react-parallax-mouse";
 
 const Main = () => {
   return (
     <>
       <section id='header' className='w-full h-screen relative justify-center'>
-        <img className='object-cover w-full h-full absolute' src='/images/castle.jpg' />
-        <div className='pl-5 pt-5'>
-          <img className='relative w-28' src='/images/logo-transparent.png' />
+
+        <img className='object-cover w-full h-full absolute' src='/images/hcm2.jpg' />
+
+        <img className='object-cover w-full h-full absolute' src='/images/hcm2-1.png' />
+
+
+        <img className='absolute w-28' src='/images/logo-transparent.png' />
+        <div className='relative text-9xl font-poppins text-white font-bold'>
+          Hayden
         </div>
-        <div className='relative flex justify-center'>
-          <H className='h-52 w-52 relative' />
-          ayden
-        </div>
+
       </section>
       <section>
         <div>test</div>
