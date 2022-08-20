90d669d313eb7e80998789f2517fa63e7c99bfdf
dnhaa <elnganha@gmail.com>
2022-08-20 22:49:01 +0700

    add mouse move parallax with parallax.js

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,20 +1,22 @@
-import React from 'react';
-import { MouseParallaxContainer, MouseParallaxChild } from "react-parallax-mouse";
+import React, { useEffect } from 'react';
+import Parallax from 'parallax-js';
 
 const Main = () => {
+  useEffect(() => {
+    const scene = document.getElementById("scene");
+    new Parallax(scene);
+  });
   return (
     <>
-      <section id='header' className='w-full h-screen relative justify-center'>
-
-        <img className='object-cover w-full h-full absolute' src='/images/hcm2.jpg' />
-
-        <img className='object-cover w-full h-full absolute' src='/images/hcm2-1.png' />
-
-
-        <img className='absolute w-28' src='/images/logo-transparent.png' />
-        <div className='relative text-9xl font-poppins text-white font-bold'>
+      <section className='w-full h-screen relative flex justify-center items-center overflow-hidden bg-black' id='scene'>
+        <img className='object-cover w-full h-full absolute ' data-depth='0.02' src='/images/hcm2.jpg' />
+        <div className='relative mt-52 w-full h-full text-center text-9xl font-poppins text-white font-bold' style={{ 'top': '100px' }}>
           Hayden
         </div>
+        <img className='object-cover w-full h-full absolute' data-depth='0.04' src='/images/hcm2-1.png' />
+
+        <img className='object-cover w-full h-full absolute scale-100' data-depth='0.05' src='/images/hcm2-2.png' />
+        <img className='relative top-52 mt-52  w-28' src='/images/logo-transparent.png' />
 
       </section>
       <section>
